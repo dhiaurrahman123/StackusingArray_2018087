b3f29d3324c1fb272618e3636ae1b413abd11efa
dhiaurrahman123 <dhiaurrahman.i.ft18@mail.umy.ac.id>
2023-06-15 10:32:56 +0700

    membuat konstruktor

--- StackusingArray_2018087.cpp
+++ StackusingArray_2018087.cpp
@@ -4,7 +4,13 @@
 using namespace std;
 
 class program {
-
+private:
+    string stack_array[5]; //jumlah array
+    int top; //menentukan bagian atas 
+public:
+    Program() {
+        top = -1;
+    }
 };
 
 int main()
