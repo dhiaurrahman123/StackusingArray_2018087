45b00e07caf93a73dc185d6c005510f2d98f2551
dhiaurrahman123 <dhiaurrahman.i.ft18@mail.umy.ac.id>
2023-06-15 10:35:51 +0700

    membuat push

--- StackusingArray_2018087.cpp
+++ StackusingArray_2018087.cpp
@@ -8,10 +8,21 @@ private:
     string stack_array[5]; //jumlah array
     int top; //menentukan bagian atas 
 public:
-    Program() {
+    program() {
         top = -1;
+    }  //membuat konstruktor
+    string push(string element) {
+        if (top == 4) {
+            cout << "Stack Full" << endl;
+            return "";
+        }
+        top++;
+        stack_array[top] = element;
+        cout << endl;
+        cout << element << "ditambahkan(pushed)." << endl;
     }
 };
+
 
 int main()
 {
